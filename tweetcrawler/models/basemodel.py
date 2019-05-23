d2ed685c387fb0fef45a615996e0978d5ccb9342
Mate <matej.valtr@gmail.com>
2019-05-23 17:47:32 +0800

    Function of saving results of scraping into database.

--- tweetcrawler/models/basemodel.py
+++ tweetcrawler/models/basemodel.py
@@ -11,23 +11,3 @@ class BaseModel(db.Model):
     created_at = db.Column(db.DateTime, default=db.func.now())
     updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now() )
 
-#     def save(self, *args, **kwargs):
-#         self.errors = []
-#         self.validate()
-
-        # if len(self.errors) == 0;
-        #     self.updated_at = datetime.datetime.now()
-        #     return super(BaseModel, self).save(@args, **kwargs)
-
-        # else:
-        #     return 0
-
-    # def validate(self):
-    #     print(
-    #         f"Warning validation method not implemented for {str(type(self))}"
-    #     )
-    #     return True
-
-    # class Meta:
-    #     database = database # still missing imported/defined
-    #     # legacy_table_names = False  <<??? what is this?
