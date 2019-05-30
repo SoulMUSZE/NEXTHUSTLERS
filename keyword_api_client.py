c0905fe73b5e583cf3e37c5e42cd6f085e079895
Husni Wahab <mdhusni@gmail.com>
2019-05-30 16:11:22 +0800

    modularized seo client

--- keyword_api_client.py
+++ keyword_api_client.py
@@ -3,6 +3,8 @@ from base64 import b64encode
 from json import loads
 from json import dumps
 
+from credentials import SEO_LOGIN, SEO_PASSWORD
+
 class RestClient:
     domain = "api.dataforseo.com"
 
@@ -32,3 +34,52 @@ class RestClient:
         else:
             data_str = dumps(data)
         return self.request(path, 'POST', data_str)
+
+
+def get_related_keywords(seed_word, depth=2, limit=15, country='US'):
+
+    client = RestClient(SEO_LOGIN, SEO_PASSWORD)
+
+    post_data = dict()
+    # you can set as "index of post_data" your ID, string, etc. we will return it with all results.
+    # post_data[random.randint(1, 30000000)] = dict(
+    post_data['NEXTHUSTLERS'] = dict(
+        keyword=seed_word,
+        country_code=country,
+        language="en",
+        depth=depth,
+        limit=limit,
+        offset=0,
+        orderby="search_volume,desc",
+        filters=[
+            ["cpc", ">", 0],
+            "or",
+            [
+                ["search_volume", ">", 0],
+                "and",
+                ["search_volume", "<=", 10000]
+            ]
+        ]
+    )
+
+    response = client.post("/v2/kwrd_finder_related_keywords_get", dict(data=post_data))
+
+    if response["status"] == "error":
+        # print("error. Code: %d Message: %s" %(response["error"]["code"], response["error"]["message"]))
+        # return render_template('keywords.html', keywords="API Error")
+        return False
+    else:
+        keywords = response['results']['NEXTHUSTLERS']['related']
+
+        if keywords == "No data":
+            # return render_template('keywords.html', keywords=response['results'])
+            return False
+        else:
+            key_list = []
+
+            for keyword in keywords:
+                key_list.append({k: keyword[k] for k in ('key', 'search_volume')})
+
+            key_list = [pair for pair in key_list if pair['key'] != seed_word]
+
+            return key_list
--- routes.py
+++ routes.py
@@ -14,7 +14,7 @@ from credentials import *
 from model import User, Tweet
 from flask_wtf import FlaskForm
 
-from keyword_api_client import RestClient
+from keyword_api_client import RestClient, get_related_keywords
 
 # import io
 # import csv
@@ -35,8 +35,12 @@ def index():
         '4': 101
     }
 
+    # handle hashtags
+    seedWord = request.args.get("seedWord")
+    
     city = request.args.get('city')
     followers = request.args.get('followers')
+
     result = User.query
     tweets = Tweet.query
 
@@ -57,16 +61,34 @@ def index():
                 filter(User.followers_count.between(followersDict.get(
                     followers)[0], followersDict.get(followers)[1]))
 
+        
+        '''
+        HANDLE HASHTAGS
+        '''
+        key_list = get_related_keywords(seedWord)
+
+        if key_list:
+            # sorted_key_list = sorted(key_list, key=lambda pair: pair['search_volume'], reverse=True)
+            max_volume = key_list[0]['search_volume']
+            pass
+            # return render_template('keywords.html', keywords=key_list, max=max_volume)
+        else:
+            pass
+            # return render_template('keywords.html', keywords=key_list)
+
+        # breakpoint()
+        # # split strings in key_list by whitespace
+        # split_key_list = [key.split() for key in key_list]
+        # #flatten list
+        # flattened_key_list = [item for sublist in split_key_list for item in sublist]
+        # # keep only unique keywords
+        # related_keywords = list(set(flattened_key_list))
+
+
     page = request.args.get('page', 1, type=int)
+
     users = result.order_by(User.followers_count.desc()
                             ).paginate(page=page, per_page=5)
-
-    # breakpoint()
-    # for user in result:
-    #     for tweet in user.tweets:
-    #         for hashtag in tweet.hashtags:
-    #             print(hashtag)
-
     
     return render_template('home.html', users=users, tweets=tweets)
 
@@ -135,74 +157,16 @@ def keywords():
     if request.method == 'POST':
 
         seedWord = request.form.get("seedWord")
-        
-        client = RestClient(SEO_LOGIN, SEO_PASSWORD)
 
-        # you can set as "index of post_data" your ID, string, etc. we will return it with all results.
-        # rnd = Random()
+        key_list = get_related_keywords(seedWord)
 
-        post_data = dict()
-        # post_data[random.randint(1, 30000000)] = dict(
-        post_data['NEXTHUSTLERS'] = dict(
-            keyword=seedWord,
-            country_code="US",
-            language="en",
-            depth=2,
-            limit=15,
-            offset=0,
-            orderby="search_volume,desc",
-            filters=[
-                ["cpc", ">", 0],
-                "or",
-                [
-                    ["search_volume", ">", 0],
-                    "and",
-                    ["search_volume", "<=", 10000]
-                ]
-            ]
-        )
+        if key_list:
+            # sorted_key_list = sorted(key_list, key=lambda pair: pair['search_volume'], reverse=True)
+            max_volume = key_list[0]['search_volume']
+            return render_template('keywords.html', keywords=key_list, max=max_volume)
 
-        response = client.post(
-            "/v2/kwrd_finder_related_keywords_get", dict(data=post_data))
-
-        if response["status"] == "error":
-            print("error. Code: %d Message: %s" %
-                  (response["error"]["code"], response["error"]["message"]))
-
-            return render_template('keywords.html', keywords="API Error")
         else:
-            keywords = response['results']['NEXTHUSTLERS']['related']
-
-            if keywords == "No data":
-                # return render_template('keywords.html', keywords = keywords)
-                return render_template('keywords.html', keywords=response['results'])
-            # print(response["results"])
-            else:
-                key_list = []
-                # breakpoint()
-                # for keyword in keywords:
-                #     key_list.append(keyword["key"])
-
-                for keyword in keywords:
-                    key_list.append({k: keyword[k]
-                                     for k in ('key', 'search_volume')})
-
-                key_list = [
-                    pair for pair in key_list if pair['key'] != seedWord]
-
-                # sorted_key_list = sorted(key_list, key=lambda pair: pair['search_volume'], reverse=True)
-                max_volume = key_list[0]['search_volume']
-
-                # breakpoint()
-                # # split strings in key_list by whitespace
-                # split_key_list = [key.split() for key in key_list]
-                # #flatten list
-                # flattened_key_list = [item for sublist in split_key_list for item in sublist]
-                # # keep only unique keywords
-                # related_keywords = list(set(flattened_key_list))
-
-                # return render_template('keywords.html', keywords = related_keywords)
-                return render_template('keywords.html', keywords=key_list, max=max_volume)
+            return render_template('keywords.html', keywords=key_list)
 
     # handle GET request
     return render_template('keywords.html')
